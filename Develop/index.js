f0fa332b26efe3d940d50f60b8575d14bdd11108
dorsariahi <dorsariahiasl@gmail.com>
2022-11-18 12:56:18 -0600

    added the message for the questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,62 +1,54 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
-        name: "Title",
+        name: "title",
         message: "what is the title of your ReadMe?"
     },
     {
         type: "input",
-        name: "Description",
+        name: "description",
         message: "what is the description of your ReadMe?"
     },
     {
         type: "input",
-        name: "Contents",
-        message: ""
+        name: "installation",
+        message: "how do you install this application?"
     },
     {
         type: "input",
-        name: "Installation",
-        message: ""
+        name: "usage",
+        message: "how do we use this application?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "what license would you like to use?",
+        choices: ["apache", "MIT"]
     },
     {
         type: "input",
-        name: "Usage",
-        message: ""
+        name: "contributing",
+        message: "what are the contribution guide lines?"
     },
     {
         type: "input",
-        name: "License",
-        message: ""
-    },
-    {
-        type: "input",
-        name: "Contributing",
-        message: ""
-    },
-    {
-        type: "input",
-        name: "Tests",
-        message: ""
-    },
-    {
-        type: "input",
-        name: "Questions",
-        message: ""
+        name: "tests",
+        message: "what are some examples of the test and how do use it?"
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
-       
-      })
+   console.log(generateMarkdown(answers))
+    })
 }
 
 // Function call to initialize app

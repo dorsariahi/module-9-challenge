b5a51739f2967ccd36c934eab8ff39a82a73e7cf
dorsariahi <dorsariahiasl@gmail.com>
2022-11-21 12:35:48 -0600

    added the if satement

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(data) {
+  if(data.license=="MIT"){
+    return "[![License:MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+  }
+  if(data.license=="Apache"){
+    return "[![License:Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]"
+  }
+ }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +20,7 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data)}
 #description
 ${data.description}
 #installation

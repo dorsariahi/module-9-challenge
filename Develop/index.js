be9cb79c5704b9c0340b8179326fcee15aca7e73
dorsariahi <dorsariahiasl@gmail.com>
2022-11-28 12:30:05 -0600

    created the readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+var fs = require('fs');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -52,12 +54,18 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) throw err;
+        console.log('succsesfully created a ReadMe file!');
+      });
+ }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
-   console.log(generateMarkdown(answers))
+        var readmeData = generateMarkdown(answers)
+        writeToFile('README.md', readmeData)
     })
 }
 
